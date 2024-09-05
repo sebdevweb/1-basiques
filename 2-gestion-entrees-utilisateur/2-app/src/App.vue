179d5dad7f8ed81f0bdacc07053f2c6f4d50c6b9
spansini <spansini@squad.fr>
2024-09-05 10:36:31 +0200

    [1-VUE-BASIQUE] 5 Gestion des entrées de l'utilisateur (SFC)

---
+++ 2-gestion-entrees-utilisateur/2-app/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const message = ref('Hell SFC Vue!')
+
+  function reverseMessage() {
+    message.value = message.value.split('').reverse().join('')
+  }
+  function notify() {
+    alert('MESSAGE AFFICHER SANS RECHARGER LA PAGE')
+  }
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <button @click="reverseMessage">REVERSE MESSAGE</button>
+  <button @click="message += '@' ">ADD '@'</button>
+  <button @click.prevent="notify">AFFICHER ALERTE</button>
+</template>
+
+<style scoped>
+
+</style>
--- 2-gestion-entrees-utilisateur/README.md
+++ 2-gestion-entrees-utilisateur/README.md
@@ -1,3 +1,4 @@
 # VUE JS : Basique
 
 - Gestion des entrées de l'utilisateur (HTML version)
+- Gestion des entrées de l'utilisateur (SFC version)
