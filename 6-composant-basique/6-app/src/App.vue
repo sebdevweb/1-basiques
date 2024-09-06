7fec44ca6675c2b54390d94e434fec6562112178
spansini <spansini@squad.fr>
2024-09-06 11:30:47 +0200

    [1-VUE-BASIQUE] 9 Composant basique (SFC)

---
+++ 6-composant-basique/6-app/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import { ref } from 'vue'
+  import TodoItem from './TodoItem.vue'
+
+  const groceryList = ref([
+    {
+      id: 0,
+      text: 'Légumes'
+    }, {
+      id: 1,
+      text: 'Fromage'
+    }, {
+      id: 2,
+      text: 'Toute autre chose comestible pour les humains'
+    }
+  ])
+</script>
+
+<template>
+  <ol>
+    <TodoItem 
+      v-for="item in groceryList"
+      :todo = "item"
+      :key = "item.id"
+    />
+  </ol>
+</template>
+
+<style scoped>
+
+</style>
