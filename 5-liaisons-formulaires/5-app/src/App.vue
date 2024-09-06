84300634f8f7894965d05766660943c916763a75
spansini <spansini@squad.fr>
2024-09-06 10:25:09 +0200

    [1-VUE-BASIQUE] 8 Liaisons dans les formulaires (SFC)

---
+++ 5-liaisons-formulaires/5-app/src/App.vue
@@ -0,0 +1,79 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const text = ref('Edite-moi')
+  const checked = ref(true)
+  const checkedNames = ref(['jean'])
+  const picked = ref('Un')
+  const selected = ref('A')
+  const multiSelect = ref(['A'])
+
+</script>
+
+<template>
+
+  <fieldset>
+    <h2>Champ de texte</h2>
+    <input type="text" v-model="text">
+    <p>{{ text }}</p>
+  </fieldset>
+
+  <fieldset>
+    <h2>Case à cocher</h2>
+    <input type="checkbox" id="checkbox" v-model="checked">
+    <label for="checkbox">Cochée : {{ checked }}</label>
+  </fieldset>
+
+  <fieldset>
+    <h2>Cases à cocher multiples</h2>
+    <input type="checkbox" id="jean" value="jean" v-model="checkedNames">
+    <label for="jean">Jean</label>
+    
+    <input type="checkbox" id="michel" value="michel" v-model="checkedNames">
+    <label for="michel">Michel</label>
+    
+    <input type="checkbox" id="brigitte" value="brigitte" v-model="checkedNames">
+    <label for="brigitte">Brigitte</label>
+
+    <p>Nom(s) coché(s) : {{ checkedNames }}</p>
+  </fieldset>
+
+  <fieldset>
+    <h2>Radio</h2>
+    <input type="radio" id="un" value="Un" v-model="picked">
+    <label for="un">Un</label>
+    <br>
+    <input type="radio" id="deux" value="Deux" v-model="picked">
+    <label for="deux">Deux</label>
+    <br>
+    <input type="radio" id="trois" value="Trois" v-model="picked">
+    <label for="trois">Trois</label>
+    <br>
+    <p>Choisi : {{ picked }}</p>
+  </fieldset>
+
+  <fieldset>
+    <h2>Sélection</h2>
+    <select v-model="selected">
+      <option disabled value="">Sélectionnez-en-un</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+    <p>Sélectionné : {{ selected }}</p>
+  </fieldset>
+
+  <fieldset>
+    <h2>Sélection multiple</h2>
+    <select v-model="multiSelect" multiple style="width: 200px">
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+  </fieldset>
+  <p>Sélectionné : {{ multiSelect }}</p>
+</template>
+
+<style scoped>
+
+</style>
--- 5-liaisons-formulaires/README.md
+++ 5-liaisons-formulaires/README.md
@@ -1,3 +1,4 @@
 # VUE JS : Basique
 
 - Liaisons et formulaires (HTML version)
+- Liaisons et formulaires (SFC version)
